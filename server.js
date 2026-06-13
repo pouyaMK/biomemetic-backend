746193b85dec737993d8d36baa5182a270983822
pouya <pouyamadankar@gmail.com>
2026-06-13 15:39:34 +0330

    wip before postgress

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const phoneRoutes = require("./routes/phoneRoutes");
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
 app.use(express.json());
 
